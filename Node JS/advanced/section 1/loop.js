04613eab4d0ba321e42f4153f11483253af321eb
badgamerbad <badgamerbad@gmail.com>
2018-04-09 23:30:20 +0530

    added node js advanced

---
+++ Node JS/advanced/section 1/loop.js
@@ -0,0 +1,15 @@
+//node myFile.js
+
+myFile.runContents();
+
+//helper function
+function shouldContinue(){
+	
+}
+
+// entire body executes in one 'tick'
+while(shouldContinue()) {
+	
+}
+
+//exit back to terminal
