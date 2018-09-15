f71be41f93082c87dfa32ea26b97ef7e4c27abae
badgamerbad <badgamerbad@gmail.com>
2018-09-15 19:30:47 +0530

    added image slider

---
+++ Web Technologies/tmp/webpack.config.js
@@ -0,0 +1,24 @@
+// webpack.config.js
+module.exports = {
+    mode: 'development',
+    entry: './main.js',
+    output: {
+      filename: 'index.js',
+      publicPath: 'dist'
+    },
+    devtool: 'inline-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
+    }
+  };
