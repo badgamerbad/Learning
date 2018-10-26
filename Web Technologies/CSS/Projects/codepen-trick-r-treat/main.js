544033cf39fa53ec0fd261344b92bf503841e162
badgamerbad <badgamerbad@gmail.com>
2018-10-26 17:47:22 +0530

    current state

--- Web Technologies/CSS/Projects/codepen-trick-r-treat/main.js
+++ Web Technologies/CSS/Projects/codepen-trick-r-treat/main.js
@@ -1,5 +1,17 @@
 require(`./index.html`);
 require(`./index.css`);
 
-var foo = "Foo"
-console.log(`${foo}`);
+class foo {
+    constructor() {
+        this.mouseCoordinates = {
+            x: 0,
+            y: 0,
+        };
+        this.init();
+    }
+    init() {
+        let body = document.getElementsByTagName(`body`);
+        let link = `https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent`;
+    }
+}
+new foo();
