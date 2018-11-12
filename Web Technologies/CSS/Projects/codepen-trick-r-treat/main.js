773a637098413670dc28444340b080de056f9c22
badgamerbad <badgamerbad@gmail.com>
2018-11-12 17:47:07 +0530

    move pumpkin on click

--- Web Technologies/CSS/Projects/codepen-trick-r-treat/main.js
+++ Web Technologies/CSS/Projects/codepen-trick-r-treat/main.js
@@ -3,7 +3,7 @@ require(`./index.css`);
 
 class foo {
     constructor() {
-        this.mouseCoordinates = {
+        this.currentElementOffset = {
             x: 0,
             y: 0,
         };
@@ -11,11 +11,49 @@ class foo {
     }
     init() {
         let body = document.querySelector(`body`);
-        let candy = document.querySelector('.candy');
-        body.addEventListener('mousemove', (e) => {
-            var foo = {}
-            // console.log(`x: ${e.x}, y: ${e.y}`)
+        let pumpkin = document.querySelector('.pumpkin');
+        body.addEventListener('click', (e) => {
+            let startX = pumpkin.offsetLeft, startY = pumpkin.offsetTop;
+            let stopX = e.x - 40, stopY = e.y;
+            let incrementX, incrementY;
+            let distanceX, distanceY;
+            let stepX, stepY;
+            if(startX < stopX) {
+                incrementX = true;
+                distanceX = startX - stopX;
+            }
+            else {
+                incrementX = false;
+                distanceX = stopX - startX;
+            }
+   
+            if(startY < stopY) {
+                incrementY = true
+            }  
+            else {
+                incrementY = false;
+            } 
+            let leapX, leapY;
+            // console.log(`startX: ${startX}, startY: ${startY}`)
+            // console.log(`stopX: ${stopX}, stopY: ${stopY}`)
+            // let timer = 0
+            // for(let i = 0; ; ){
+            //     leapY += step;
+            //     if (leapY > stopY)
+            //         leapY = stopY;
+            //     setTimeout( ()=> {
+            //         pumpkin.style.left = `${leapX}px`;
+            //         pumpkin.style.top = `${leapY}px`;
+            //     }, timer);
+            //     timer++;
+            // }
         });
+    }
+    currentOffset(element) {
+        this.currentElementOffset = {
+            x: 0,
+            y: 0,
+        };
     }
 }
 new foo();
