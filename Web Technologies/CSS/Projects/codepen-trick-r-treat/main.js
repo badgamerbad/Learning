e325b23337c512334625884d4a21c468345df3d4
badgamerbad <badgamerbad@gmail.com>
2018-11-13 12:40:26 +0530

    moving pumpkin

--- Web Technologies/CSS/Projects/codepen-trick-r-treat/main.js
+++ Web Technologies/CSS/Projects/codepen-trick-r-treat/main.js
@@ -1,59 +1,93 @@
 require(`./index.html`);
 require(`./index.css`);
 
-class foo {
+class hungry_pumpkin {
     constructor() {
-        this.currentElementOffset = {
-            x: 0,
-            y: 0,
-        };
         this.init();
     }
     init() {
         let body = document.querySelector(`body`);
         let pumpkin = document.querySelector('.pumpkin');
+        let message = document.querySelector(`.message`);
         body.addEventListener('click', (e) => {
+            message.style.display = `none`;
+            pumpkin.classList.remove('close-mouth');
+            pumpkin.classList.add('open-mouth');
             let startX = pumpkin.offsetLeft, startY = pumpkin.offsetTop;
             let stopX = e.x - 40, stopY = e.y;
-            let incrementX, incrementY;
             let distanceX, distanceY;
             let stepX, stepY;
+            let leapX, leapY;
+            let speed;
             if(startX < stopX) {
-                incrementX = true;
-                distanceX = startX - stopX;
+                distanceX = stopX - startX;
+                stepX = Math.round( distanceX / 25 );
+                leapX = startX + stepX;
             }
             else {
-                incrementX = false;
-                distanceX = stopX - startX;
+                distanceX = startX - stopX;
+                stepX = Math.round( distanceX / 25 );
+                leapX = startX - stepX;
             }
-   
             if(startY < stopY) {
-                incrementY = true
+                distanceY = stopY - startY;
+                stepY = Math.round( distanceY / 25 );
+                leapY = startY + stepY;
             }  
             else {
-                incrementY = false;
+                distanceY = startY - stopY;
+                stepY = Math.round( distanceY / 25 );
+                leapY = startY - stepY;
             } 
-            let leapX, leapY;
-            // console.log(`startX: ${startX}, startY: ${startY}`)
-            // console.log(`stopX: ${stopX}, stopY: ${stopY}`)
-            // let timer = 0
-            // for(let i = 0; ; ){
-            //     leapY += step;
-            //     if (leapY > stopY)
-            //         leapY = stopY;
-            //     setTimeout( ()=> {
-            //         pumpkin.style.left = `${leapX}px`;
-            //         pumpkin.style.top = `${leapY}px`;
-            //     }, timer);
-            //     timer++;
-            // }
+            
+            if(distanceX > distanceY) {
+                speed = Math.round( distanceX / 100 );
+                if( speed >= 20 ) speed = 20;
+            }
+            else {
+                speed = Math.round( distanceY / 100 );
+                if( speed >= 20 ) speed = 20;
+            }
+            
+            console.log(`startX: ${startX}, startY: ${startY}`)
+            console.log(`stopX: ${stopX}, stopY: ${stopY}`)
+            console.log(`distanceX: ${distanceX}, distanceY: ${distanceY}`)
+            console.log(`stepX: ${stepX}, stepY: ${stepY}`)
+            console.log(`leapX: ${leapX}, leapY: ${leapY}`)
+
+            let timer = 0
+            let continue_while = true;
+            while(continue_while){
+                if(startX < stopX) {
+                    leapX += stepX;
+                    if(leapX > stopX)
+                        leapX = stopX;
+                }
+                else {
+                    leapX -= stepX;
+                    if(leapX < stopX)
+                        leapX = stopX;
+                }
+                if(startY < stopY) {
+                    leapY += stepY;
+                    if(leapY > stopY)
+                        leapY = stopY;
+                }  
+                else {
+                    leapY -= stepY;
+                    if(leapY < stopY)
+                        leapY = stopY;
+                }
+
+                setTimeout( `document.querySelector('.pumpkin').style.top = '${leapY}px'; document.querySelector('.pumpkin').style.left = '${leapX}px';`, timer * speed);
+                timer++;
+
+                if(leapX == stopX || leapY == stopY) {
+                    continue_while = false;
+                    setTimeout( `document.querySelector('.pumpkin').classList.add('close-mouth'); document.querySelector('.pumpkin').classList.remove('open-mouth')`, timer * speed);
+                } 
+            }
         });
     }
-    currentOffset(element) {
-        this.currentElementOffset = {
-            x: 0,
-            y: 0,
-        };
-    }
 }
-new foo();
+new hungry_pumpkin();
