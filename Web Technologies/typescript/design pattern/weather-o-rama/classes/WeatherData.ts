dfc558d77e058a02f295a0d05641c8ebb3fc304c
badgamerbad <badgamerbad@gmail.com>
2019-04-30 12:26:21 +0530

    typescript tutorial

---
+++ Web Technologies/typescript/design pattern/weather-o-rama/classes/WeatherData.ts
@@ -0,0 +1,14 @@
+class WeatherData {
+    public getTemperature(): String {
+        return "This is Temperature!!!";
+    }
+    public getHumidity(): String {
+        return "This is Humidity!!!";
+    }
+    public getPressure(): String {
+        return "This is Pressure!!!";
+    }
+    public measurementsChanged(): String {
+        return "New Measurements!!!";
+    }
+}
