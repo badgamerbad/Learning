fba542be4aaf30b643d50f01e05b9b2b6b472e76
Vipul Dessai <badgamerbad@gmail.com>
2019-11-27 23:21:54 +0530

    logo x

--- Web Technologies/JavaScript/Canvas/projects/project x/index.js
+++ Web Technologies/JavaScript/Canvas/projects/project x/index.js
@@ -4,9 +4,12 @@ function draw() {
         let ctx = canvas.getContext('2d');
         ctx.translate(1, 0);
         ctx.beginPath();
-        ctx.lineTo(100, 250);
-        ctx.lineTo(300, 250);
-        ctx.arc(400, 250, 50, 10, 60, true);
+        // ctx.lineTo(100, 250);
+
+        // ctx.arcTo(300, 200, 200, 150, 50);
+        ctx.arc(300, 250, 10, 0, Math.PI * 2, true);
+        ctx.bezierCurveTo(50, 100, 300, 10, 10, 20);
+        // ctx.arc(300, 150, 100, 0, 3 * Math.PI / 2, true);
         // ctx.closePath();
         ctx.stroke();
         // requestAnimationFrame(draw);
