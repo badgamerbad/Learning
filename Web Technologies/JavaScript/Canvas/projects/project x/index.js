468f262067b8721838c88a21ac01186df2a6e926
Vipul Dessai <badgamerbad@gmail.com>
2019-11-28 23:07:19 +0530

    logo x hidden coords

--- Web Technologies/JavaScript/Canvas/projects/project x/index.js
+++ Web Technologies/JavaScript/Canvas/projects/project x/index.js
@@ -4,22 +4,30 @@ function draw() {
         let ctx = canvas.getContext('2d');
         ctx.translate(5, 5);
         ctx.beginPath();
-        ctx.arc(200, 350, 50, 3 * Math.PI / 2, Math.PI, false);
-        ctx.arc(100, 250, 50, 0, Math.PI / 2, true);
+        ctx.arc(300, 300, 50, 3 * Math.PI / 2, Math.PI, false);
+        ctx.arc(200, 200, 50, 0, Math.PI / 2, true);
         ctx.closePath();
 
-        ctx.moveTo(200, 350);
-        ctx.strokeText(` 1 200, 350`, 200, 350);
-        ctx.arc(200, 350, 1, 0, 2 * Math.PI);
-        
-        ctx.moveTo(100, 250);
-        ctx.strokeText(` 2 100, 250`, 100, 250);
-        ctx.arc(100, 250, 1, 0, 2 * Math.PI);
+        // showCoordinates(ctx);
 
         ctx.stroke();
         
         // requestAnimationFrame(draw);
     }
+}
+
+function showCoordinates(ctx) {
+    ctx.moveTo(300, 300);
+    ctx.strokeText(` 1 300, 300`, 300, 300);
+    ctx.arc(300, 300, 1, 0, 2 * Math.PI);
+    
+    ctx.moveTo(200, 200);
+    ctx.strokeText(` 2 200, 200`, 200, 200);
+    ctx.arc(200, 200, 1, 0, 2 * Math.PI);
+
+    ctx.moveTo(250, 250);
+    ctx.strokeText(` 3 250, 250`, 250, 250);
+    ctx.arc(250, 250, 1, 0, 2 * Math.PI);
 }
 
 /**
