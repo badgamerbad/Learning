0116b12c42e3ca598d5485bbab4989b2a5c39351
Vipul Dessai <badgamerbad@gmail.com>
2019-11-28 22:48:25 +0530

    logo x closed, refer radians to degree

--- Web Technologies/JavaScript/Canvas/projects/project x/index.js
+++ Web Technologies/JavaScript/Canvas/projects/project x/index.js
@@ -2,16 +2,22 @@ function draw() {
     let canvas = document.getElementById("logo");
     if(canvas.getContext) {
         let ctx = canvas.getContext('2d');
-        ctx.translate(1, 0);
+        ctx.translate(5, 5);
         ctx.beginPath();
-        // ctx.lineTo(100, 250);
+        ctx.arc(200, 350, 50, 3 * Math.PI / 2, Math.PI, false);
+        ctx.arc(100, 250, 50, 0, Math.PI / 2, true);
+        ctx.closePath();
 
-        // ctx.arcTo(300, 200, 200, 150, 50);
-        ctx.arc(300, 250, 10, 0, Math.PI * 2, true);
-        ctx.bezierCurveTo(50, 100, 300, 10, 10, 20);
-        // ctx.arc(300, 150, 100, 0, 3 * Math.PI / 2, true);
-        // ctx.closePath();
+        ctx.moveTo(200, 350);
+        ctx.strokeText(` 1 200, 350`, 200, 350);
+        ctx.arc(200, 350, 1, 0, 2 * Math.PI);
+        
+        ctx.moveTo(100, 250);
+        ctx.strokeText(` 2 100, 250`, 100, 250);
+        ctx.arc(100, 250, 1, 0, 2 * Math.PI);
+
         ctx.stroke();
+        
         // requestAnimationFrame(draw);
     }
 }
