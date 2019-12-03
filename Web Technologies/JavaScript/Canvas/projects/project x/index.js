5ec29c1b0b64d68a4ec774669e18ac231e14f5d9
Vipul Dessai <badgamerbad@gmail.com>
2019-12-03 23:55:46 +0530

    canvas degrees reference, animations function

--- Web Technologies/JavaScript/Canvas/projects/project x/index.js
+++ Web Technologies/JavaScript/Canvas/projects/project x/index.js
@@ -1,29 +1,36 @@
-function draw() {
-    let canvas = document.getElementById("logo");
+const canvas = document.getElementById("logo");
+const ctx = canvas.getContext('2d');
+function init() {
     if(canvas.getContext) {
-        let ctx = canvas.getContext('2d');
-        ctx.translate(5, 5);
         ctx.beginPath();
-        ctx.arc(300, 300, 50, 3 * Math.PI / 2, Math.PI, false);
         ctx.arc(200, 200, 50, 0, Math.PI / 2, true);
+        ctx.arc(300, 300, 50, 3 * Math.PI / 2, Math.PI, false);
         ctx.closePath();
 
         // showCoordinates(ctx);
 
+        draw();
+
         ctx.stroke();
+
         
-        // requestAnimationFrame(draw);
     }
 }
 
+function draw() {
+    ctx.clearRect(0, 0, 500, 500);
+    ctx.translate(1, 1);
+    requestAnimationFrame(draw);
+}
+
 function showCoordinates(ctx) {
-    ctx.moveTo(300, 300);
-    ctx.strokeText(` 1 300, 300`, 300, 300);
-    ctx.arc(300, 300, 1, 0, 2 * Math.PI);
-    
     ctx.moveTo(200, 200);
-    ctx.strokeText(` 2 200, 200`, 200, 200);
+    ctx.strokeText(` 1 200, 200`, 200, 200);
     ctx.arc(200, 200, 1, 0, 2 * Math.PI);
+
+    ctx.moveTo(300, 300);
+    ctx.strokeText(` 2 300, 300`, 300, 300);
+    ctx.arc(300, 300, 1, 0, 2 * Math.PI);
 
     ctx.moveTo(250, 250);
     ctx.strokeText(` 3 250, 250`, 250, 250);
