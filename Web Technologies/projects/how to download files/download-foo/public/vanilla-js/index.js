7a0a50d8a48361f3a466e5262b618d0e7088efb7
Vipul Dessai <badgamerbad@gmail.com>
2019-12-15 21:36:10 +0530

    added XMLHttpRequest example in download file project

--- Web Technologies/projects/how to download files/download-foo/public/vanilla-js/index.js
+++ Web Technologies/projects/how to download files/download-foo/public/vanilla-js/index.js
@@ -1,4 +1,4 @@
-var downloadFileButton = document.getElementById("download-file-button");
+var downloadFileButton = document.getElementById("download-file-button-fetch");
 downloadFileButton.addEventListener(
     "click", 
     function(event) {
@@ -40,5 +40,48 @@ downloadFileButton.addEventListener(
         .catch(function(e) {
             console.log(e)
         })
+    }
+);
+
+var downloadFileButton = document.getElementById("download-file-button-xml-http");
+downloadFileButton.addEventListener(
+    "click", 
+    function(event) {
+        var xhr = new XMLHttpRequest();
+        xhr.open(
+            "GET", 
+            "/download-file", 
+            true
+        );
+        xhr.responseType = "arraybuffer";
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('Accept', 'application/vnd.ms-excel');
+        xhr.send();
+        xhr.onload = function() {
+            var buffer = xhr.response;
+            var contentDisposition = xhr.getResponseHeader('Content-disposition');
+            var fileName = contentDisposition.replace("attachment; filename=\"", "");
+            fileName = fileName.replace("\"", "");
+            try {
+                var blob = new Blob([buffer], { type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" });
+                if (navigator.msSaveBlob) {
+                    //for Internet explorer
+                    window.navigator.msSaveBlob(blob, fileName);
+                }
+                else {
+                    var a = document.createElement("a");
+                    document.body.appendChild(a);
+                    a.style = "display: none";
+                    var url = window.URL.createObjectURL(blob);
+                    a.href = url;
+                    a.download = fileName;
+                    a.click();
+                    a.remove();
+                }
+            }
+            catch (e) {
+                console.log(e);
+            }
+        }
     }
 );
